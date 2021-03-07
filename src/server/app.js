340682cab7b43643246a9e9b56e64d0d3c0bff32
zhihengchang <zhihengchang602@gmail.com>
2021-03-07 15:21:20 -0500

    server: changz add app.js

---
+++ src/server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan"); //a logger module
+const path = require("path");
+
+//Routes
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+//Get views path
+const parentDir = path.join(__dirname, '../');
+const viewDir = path.join(parentDir, 'client/views');
+//Set views path
+app.set('views', viewDir);
+//Set EJS
+app.set('view engine', 'ejs'); 
+
+//Mounting
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api/v1/users", userRoutes);
+
+module.exports = app;
