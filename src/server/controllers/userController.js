104bb7d6fa905778f61a9bc9816b620590477f3e
zhihengchang <zhihengchang602@gmail.com>
2021-03-07 15:33:52 -0500

    server: changz add userController

---
+++ src/server/controllers/userController.js
@@ -0,0 +1,41 @@
+/**
+ * User Controller
+ * 
+ * used by: userRoutes.js
+ * descriptions: provides user management functions
+ */
+
+//utils
+const util = require("../../util/server_utilities");
+const logger = require("../../util/logger");
+
+//Models
+
+
+//Middlewares
+
+
+//Controller Functions
+exports.getUser = function(req, res){
+
+}
+
+exports.getAllUser = function(req, res){
+
+}
+
+exports.createUser = async function(req, res){
+  
+}
+
+exports.deleteUser = function(req, res){
+
+}
+
+exports.updateUser = function(req, res){
+
+}
+
+exports.validateUser = function(req, res){
+
+}
