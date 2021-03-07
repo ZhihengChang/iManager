099566b164983f50d497284e53f176e887dc6b6d
zhihengchang <zhihengchang602@gmail.com>
2021-03-07 15:59:09 -0500

    server: changz add createUser function

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -6,11 +6,11 @@
  */
 
 //utils
-const util = require("../../util/server_utilities");
-const logger = require("../../util/logger");
+const util = require('../../util/server_utilities');
+const logger = require('../../util/logger');
 
 //Models
-
+const User = require('../models/userModel');
 
 //Middlewares
 
@@ -25,7 +25,18 @@ exports.getAllUser = function(req, res){
 }
 
 exports.createUser = async function(req, res){
-  
+    try{
+        const newUser = await User.create(req.body);
+        util.sendResponse(res, 200, {
+            status: 'success',
+            data: { user: newUser }
+        });
+    }catch(err){
+        util.sendResponse(res, 400, {
+            status: 'fail',
+            message: err
+        });
+    }  
 }
 
 exports.deleteUser = function(req, res){
