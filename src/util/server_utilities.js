c06de0f075e733c06ecd7090b051ef0a5f33c188
zhihengchang <zhihengchang602@gmail.com>
2021-03-10 22:51:23 -0500

    server: changz add server_utilities documentation

--- src/util/server_utilities.js
+++ src/util/server_utilities.js
@@ -7,6 +7,12 @@
 
 const logger = require("./logger");
 
+/**
+ * Send json response with status code
+ * @param {Response} res 
+ * @param {Number} statusCode 
+ * @param {Object} data 
+ */
 exports.sendResponse = function(res, statusCode, data){
     res.status(statusCode).json(data);
 }
